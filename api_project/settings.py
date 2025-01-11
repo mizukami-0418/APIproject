31eac3f19828c55156a75647f90275661f75e69d
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-01-11 11:00:38 +0900

    ローカル用のcorsの設定を追加

--- api_project/settings.py
+++ api_project/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',  # DRFの追加
     'api',  # 作成したアプリの追加
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",  # フロントエンドのURL
+    "http://localhost:5500",  # ローカル環境用
+]
+
 
 ROOT_URLCONF = 'api_project.urls'
 
